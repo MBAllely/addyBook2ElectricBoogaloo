0df682ef4b5df6f595dc62adb9373b3554765e06
jon sweeney and marika allely <jonsweeney26@gmail.com>
2016-01-26 11:16:05 -0800

    add some SICK TOGGLES

--- js/scripts.js
+++ js/scripts.js
@@ -28,22 +28,10 @@ function resetFields() {
 };
 
 $(document).ready(function() {
-  $("#add-address").click(function() {
-    $("#new-address").append('<div class="new-address" id="billing-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
+  $("#add-address").click("a", function() {
+    $("#billing-address").slideToggle("slow");
 
+    // $("#new-address").toggle("slow");
   });
 
   $("form#new-contact").submit(function(event) {
